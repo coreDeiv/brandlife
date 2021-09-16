daccc95513eb9b5b7469276a99956cd36838d5e7
david_fernando_torres_parra <dftp93@gmail.com>
2021-09-16 18:58:32 -0500

    [Brandlife][Master] Update and add section jumbotron
    The jumbotron is the first slider of the page

---
+++ build/js/script.js
@@ -0,0 +1,2 @@
+"use strict";$(".pj-slider").slick({dots:!0,infinite:!0,slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:3e3});
+//# sourceMappingURL=maps/script.js.map
---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$('.pj-slider').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
