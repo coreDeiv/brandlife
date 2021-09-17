3bb884aa65218f4df2512de39597df354e54eb14
david_fernando_torres_parra <dftp93@gmail.com>
2021-09-17 13:16:47 -0500

    [Brandlife][Master] Update and add grid gallery instagram images

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,20 @@ $('.pj-slider').slick({
   autoplay: true,
   autoplaySpeed: 3000,
 });
+
+// Instagram Grid
+const images = document.querySelector('#pi-body_list');
+const list = Array.from(images.children);
+let newlist;
+
+list.sort((a, b) => {
+  return -1 + Math.random() * 3;
+});
+
+while (images.children.length > 0) {
+  images.removeChild(images.children[0]);
+}
+
+list.forEach((e) => {
+  images.appendChild(e);
+});
